621b0fdf94788366eb524de6464824c9b69d2637
dayyan555 <diyyan555@gmail.com>
2023-11-25 18:55:18 +0500

    Nwe updated block and merkletree

--- block/merkleTree.go
+++ block/merkleTree.go
@@ -16,12 +16,6 @@ type MerkleTree struct {
 	rows []Row
 }
 
-// MerklePathElement represents a hash value and its order in the Merkle Path
-// type MerklePathElement struct {
-// 	hash                    string
-// 	useFirstInConcatenation bool
-// }
-
 // NewMerkleTree constructs a Merkle Tree from a list of transactions
 func NewMerkleTree(transactions TransactionList) MerkleTree {
 	var row Row
@@ -49,6 +43,63 @@ func NewMerkleTree(transactions TransactionList) MerkleTree {
 func (tree MerkleTree) MerkleRoot() string {
 	return tree.rows[len(tree.rows)-1][0]
 }
+
+// adjustRows ensures that each row has an even number of nodes
+func (tree MerkleTree) adjustRows() {
+	for level := len(tree.rows) - 1; level >= 0; level-- {
+		row := tree.rows[level]
+		//fmt.Println("Level", level, "has", len(row), "nodes")
+		if len(row)%2 != 0 && len(row) > 1 {
+			//fmt.Println("Adjusted row length to ensure even nodes in level", level)
+			lastNode := row[len(row)-1]
+			//fmt.Println("Duplicating node:", lastNode)
+			tree.rows[level] = append(tree.rows[level], lastNode)
+		}
+	}
+}
+
+// makeRowAbove creates a new row above the current row
+func (tree MerkleTree) makeRowAbove(below Row) Row {
+	size := int(math.Ceil(float64(len(below)) / 2.0))
+	row := make(Row, size)
+	for i := range row {
+		leftChild := i * 2
+		rightChild := leftChild + 1
+		if rightChild <= len(below)-1 {
+			row[i] = tree.joinAndHash(below[leftChild], below[rightChild])
+		} else {
+			row[i] = tree.joinAndHash(below[leftChild], below[leftChild])
+		}
+	}
+	return row
+}
+
+// joinAndHash concatenates two strings and returns the SHA256 hash of the concatenated string
+func (tree MerkleTree) joinAndHash(a, b string) string {
+	data := strings.Join([]string{a, b}, "")
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
+
+// isComplete returns true if the tree is complete
+func (tree MerkleTree) isComplete() bool {
+	return len(tree.rows[len(tree.rows)-1]) == 1
+}
+
+// Display displays the Merkle Tree
+func (tree MerkleTree) Display() {
+	fmt.Println("Merkle Tree:")
+	for i, row := range tree.rows {
+		fmt.Printf("Level %d: %d nodes\n", i, len(row))
+		fmt.Println(strings.Join(row, ", "))
+	}
+}
+
+// MerklePathElement represents a hash value and its order in the Merkle Path
+// type MerklePathElement struct {
+// 	hash                    string
+// 	useFirstInConcatenation bool
+// }
 
 // MerklePathForLeaf returns the Merkle Path for a specific leaf in the tree
 // func (tree MerkleTree) MerklePathForLeaf(leafIndex int) MerklePath {
@@ -80,50 +131,5 @@ func (tree MerkleTree) MerkleRoot() string {
 // 	return
 // }
 
-func (tree MerkleTree) adjustRows() {
-	for level := len(tree.rows) - 1; level >= 0; level-- {
-		row := tree.rows[level]
-		//fmt.Println("Level", level, "has", len(row), "nodes")
-		if len(row)%2 != 0 && len(row) > 1 {
-			//fmt.Println("Adjusted row length to ensure even nodes in level", level)
-			lastNode := row[len(row)-1]
-			//fmt.Println("Duplicating node:", lastNode)
-			tree.rows[level] = append(tree.rows[level], lastNode)
-		}
-	}
-}
-func (tree MerkleTree) makeRowAbove(below Row) Row {
-	size := int(math.Ceil(float64(len(below)) / 2.0))
-	row := make(Row, size)
-	for i := range row {
-		leftChild := i * 2
-		rightChild := leftChild + 1
-		if rightChild <= len(below)-1 {
-			row[i] = tree.joinAndHash(below[leftChild], below[rightChild])
-		} else {
-			row[i] = tree.joinAndHash(below[leftChild], below[leftChild])
-		}
-	}
-	return row
-}
-
-func (tree MerkleTree) joinAndHash(a, b string) string {
-	data := strings.Join([]string{a, b}, "")
-	hash := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(hash[:])
-}
-
-func (tree MerkleTree) isComplete() bool {
-	return len(tree.rows[len(tree.rows)-1]) == 1
-}
-
 // MerklePath represents the path in the Merkle Tree
 //type MerklePath []MerklePathElement
-
-func (tree MerkleTree) Display() {
-	fmt.Println("Merkle Tree:")
-	for i, row := range tree.rows {
-		fmt.Printf("Level %d: %d nodes\n", i, len(row))
-		fmt.Println(strings.Join(row, ", "))
-	}
-}
